1ce864ddfa589e168c327112b22f09ee208e1a1d
yub-ing <3153825216@qq.com>
2025-06-18 20:58:15 +0800

    Delete src/onshop directory

--- src/onshop/src/main/java/com/project/platform/dto/UpdatePasswordDTO.java
+++
@@ -1,9 +0,0 @@
-package com.project.platform.dto;
-
-import lombok.Data;
-
-@Data
-public class UpdatePasswordDTO {
-    private String oldPassword;
-    private String newPassword;
-}
--- src/onshop/web/src/views/admin/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <el-row :gutter="20">
-        首页
-    </el-row>
-
-  </div>
-</template>
-<script setup></script>
